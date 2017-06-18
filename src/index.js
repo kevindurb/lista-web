ab6e6182e1db7f576d84a516cef5e6a0d31d1183
Kevin Durbin <kevindurb@gmail.com>
2017-06-17 23:38:25 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div>hello world</div>,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
